8bda166d1ab7be152136c1f5d81c12fb6881cfc2
dylandy.chang <steven80919@gmail.com>
2014-09-11 20:14:45 +0800

    add ubuntu font and correct time's format

--- chrome/js/manager.js
+++ chrome/js/manager.js
@@ -4,7 +4,20 @@ var set_current_time = function(){
   var month = current.getMonth()+1;
   var hour = current.getHours();
   var minute = current.getMinutes();
-  $('#current_time')[0].innerHTML = hour.toString() + ":" + minute.toString();
+
+  //show time in correct format
+  if( hour.toString().length == 1){
+    if( minute.toString().length == 1){
+      var time_result = "0" + hour.toString() + ":" + "0" + minute.toString();
+    }
+    var time_result = "0" + hour.toString + ":" + minute.toString();
+  }else if( minute.toString().length == 1 && hour.toString().length != 1){
+    var time_result = "0" + hour.toString() + ":" + minute.toString();
+  }else{
+    var time_result = hour.toString() + ":" + minute.toString();
+  }
+
+  $('#current_time')[0].innerHTML = time_result;
 }
 jQuery(function($){
   set_current_time();
