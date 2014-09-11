a26819f87bba22dbc34c8021fc2bddaae0196c0b
dylandy.chang <steven80919@gmail.com>
2014-09-11 21:29:40 +0800

    setting font family by default font across platform and get the greeting word correct by time

--- chrome/js/manager.js
+++ chrome/js/manager.js
@@ -16,14 +16,32 @@ var set_current_time = function(){
     }
     var time_result = "0" + hour.toString + ":" + minute.toString();
   }else if( minute.toString().length == 1 && hour.toString().length != 1){
-    var time_result = "0" + hour.toString() + ":" + minute.toString();
+    var time_result = hour.toString() + ":" + "0" + minute.toString();
   }else{
     var time_result = hour.toString() + ":" + minute.toString();
   }
 
   $('#current_time')[0].innerHTML = time_result;
   $('#current_date')[0].innerHTML = monthNames[ month -1 ] + " , " + day + " , " + year;
+
 }
+
+var set_greeting_word = function(){
+  var current = new Date;
+  var hour = current.getHours();
+
+  if( hour > 5 && hour < 12 ){
+    $('#greeting_word')[0].innerHTML = "早安！";
+  }else if( hour >= 12 && hour < 14 ){
+    $('#greeting_word')[0].innerHTML = "午安！";
+  }else if( hour >= 14 && hour < 18 ){
+    $('#greeting_word')[0].innerHTML = "下午好！";
+  }else{
+    $('#greeting_word')[0].innerHTML = "晚安！";
+  }
+}
+
 jQuery(function($){
   set_current_time();
+  set_greeting_word();
 });
