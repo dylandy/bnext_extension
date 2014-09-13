a565d1cc17262d1f0dea96069b7bfa6cc3999f74
dylandy.chang <steven80919@gmail.com>
2014-09-13 22:09:27 +0800

    add permission for storage and save setting in variable

---
+++ chrome/js/option_page.js
@@ -0,0 +1,18 @@
+var save_change = function(){
+  var my_name = $('#name')[0].value;
+}
+
+var set_dropdown = function(){
+  $('.dropdown-menu li a').click(function(){
+    var where = $(this).text();
+  });
+}
+
+jQuery(function($){
+  set_dropdown();
+  $('#summit').click(function(){
+    save_change();
+    $(this)[0].innerHTML = "已儲存";
+    $(this).addClass("disabled");
+  });
+});
