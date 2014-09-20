57c396de233b2052d1f56c9c6f571fd6a3481850
dylandy.chang <steven80919@gmail.com>
2014-09-20 14:19:06 +0800

    improve UX by letting time to now setting

--- chrome/js/option_page.js
+++ chrome/js/option_page.js
@@ -23,26 +23,60 @@ var set_dropdown = function(){
 }
 
 var set_time_format = function(){
-  var now_setting = 0;
-  $('#myonoffswitch').click(function(){
-    if(now_setting == 0){
-      now_setting = "1";
-      chrome.storage.local.set(
-        {"time_format" : now_setting },
-        function(){
-          console.log("time format set to 24");
-        }
-      );
+  now_format="";
+  chrome.storage.local.get("time_format" , function(result){
+    now_format = result.time_format;
+    if(now_format){
+      if(now_format == 0){
+        $('.onoffswitch-inner').attr('data-before' , "12");
+        $('.onoffswitch-inner').attr('data-after' , "24");
+        $('#myonoffswitch').click(function(){
+            now_format = "1";
+            chrome.storage.local.set(
+              {"time_format" : now_format },
+              function(){
+                console.log("time format set to 24");
+              }
+            );
+          });
+        }else{
+            $('.onoffswitch-inner').attr('data-before' , "24");
+            $('.onoffswitch-inner').attr('data-after' , "12");
+            $('#myonoffswitch').click(function(){
+            now_format = "0";
+            chrome.storage.local.set(
+              {"time_format" : now_format },
+              function(){
+                console.log("time format set to 12");
+              }
+            );
+          });
+      }
     }else{
-      now_setting = "0";
-      chrome.storage.local.set(
-        {"time_format" : now_setting },
-        function(){
-          console.log("time format set to 12");
+      var now_setting = 0;
+      $('#myonoffswitch').click(function(){
+        if(now_setting == 0){
+          now_setting = "1";
+          chrome.storage.local.set(
+            {"time_format" : now_setting },
+            function(){
+              console.log("time format set to 24");
+            }
+          );
+        }else{
+          now_setting = "0";
+          chrome.storage.local.set(
+            {"time_format" : now_setting },
+            function(){
+              console.log("time format set to 12");
+            }
+          );
         }
-      );
+      });
     }
   });
+
+
 
 }
 
