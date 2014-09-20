1e31760b257ff3fb2faafc4dfb08f8a2dd0f4d0b
Dylandy Chang <steven80919@gmail.com>
2014-09-20 21:59:42 +0800

    change api from direct to own server

--- chrome/js/dynamic.js
+++ chrome/js/dynamic.js
@@ -1,11 +1,10 @@
 var get_weather(){
   $.ajax({
       type: "GET",
-      url: "http://opendata.cwb.gov.tw/opendata/MFC/F-C0032-001.xml",
-      dataType: "xml",
-      success: function(xml){
-        $(xml).find("location").each(function(){
-
+      url: "http://api.bnext.com.tw/weather",
+      dataType: "json",
+      success: function(json){
+        $(json).find("location").each(function(){
 
         });
       },
