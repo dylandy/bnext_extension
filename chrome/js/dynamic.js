f6e9eee8d35292a43b8b37187004946b08c054b0
dylandy.chang <steven80919@gmail.com>
2014-10-01 19:41:51 +0800

    add icon to when getting data from server

--- chrome/js/dynamic.js
+++ chrome/js/dynamic.js
@@ -7,9 +7,29 @@ var get_weather = function(){
           var i = 0;
           $(json).each(function(){
             if( this.name === $('#city').html()){
-              $("#wx"+ i)[0].innerHTML = this.wx;
               $("#maxt"+ i )[0].innerHTML = this.maxt;
               $("#mint"+ i)[0].innerHTML = this.mint;
+
+              $("#wx" + i).attr("title" , this.wx );
+
+              if( this.wx.match("多雲") || this.wx.match("陰") ){
+                $("#wx" + i ).attr("src" , "resource/img/weather/clouds-64.gif");
+              }
+              if( this.wx.match("短暫陣雨")){
+                $("#wx" + i ).attr("src" , "resource/img/weather/little-rain-64.gif");
+              }
+              if( this.wx.match("雷陣雨")){
+                $("#wx" + i ).attr("src" , "resource/img/weather/cloud-lighting-64.gif");
+              }
+              if( this.wx.match("有雨")){
+                $("#wx" + i ).attr("src" , "resource/img/weather/rain-64.gif");
+              }
+              if( this.wx.match("晴")){
+                $("#wx" + i ).attr("src" , "resource/img/weather/sun-64.gif");
+              }
+              if( this.wx.match("晴時多雲") || this.wx.match("多雲時晴")){
+                $("#wx" + i ).attr("src" , "resource/img/weather/partly-cloudy-day-64.gif");
+              }
               i++;
             }
           });
